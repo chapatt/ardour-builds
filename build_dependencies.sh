e06bcf779cd0c68eec1d340d62202a0680e4108b
Chase Patterson <chapatt@gmail.com>
2022-01-23 19:15:27 -0500

    Add sudo to install commands

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -9,28 +9,28 @@ tar xf ../dependency_sources/autoconf-2.69.tar.xz
 pushd autoconf-2.69
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/automake-1.14.1.tar.gz
 pushd automake-1.14.1
 ./configure
 make
-make install
+sudo make install
 popd
 
 # tar xf ../dependency_sources/cmake-2.8.12.2.tar.gz
 # pushd cmake-2.8.12.2
 # ./configure
 # make
-# make install
+# sudo make install
 # popd
 
 tar xf ../dependency_sources/bison-3.1.tar.xz
 pushd bison-3.1
 ./configure
 make
-make install
+sudo make install
 popd
 
 # tar xf ../dependency_sources/boost_1_68_0.tar.bz2
@@ -41,77 +41,77 @@ tar xf ../dependency_sources/cppunit-1.13.2.tar.gz
 pushd cppunit-1.13.2
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/curl-7.66.0.tar.xz
 pushd curl-7.66.0
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/expat-2.1.0.tar.gz
 pushd expat-2.1.0
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/fftw-3.3.8.tar.gz
 pushd fftw-3.3.8
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/flac-1.3.2.tar.xz
 pushd flac-1.3.2
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/flex-2.6.4.tar.gz
 pushd flex-2.6.4
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/fontconfig-2.13.1.tar.bz2
 pushd fontconfig-2.13.1
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/freetype-2.9.tar.gz
 pushd freetype-2.9
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/gettext-0.19.3.tar.gz
 pushd gettext-0.19.3
 ./configure --disable-java --disable-native-java
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/gnome-common-2.34.0.tar.bz2
 pushd gnome-common-2.34.0
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/harfbuzz-0.9.35.tar.bz2
 pushd harfbuzz-0.9.35
 ./configure
 make
-make install
+sudo make install
 popd
 
 # Depends on perl XML::Parser
@@ -119,28 +119,28 @@ tar xf ../dependency_sources/intltool-0.50.2.tar.gz
 pushd intltool-0.50.2
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/jpeg-9a.tar.gz
 pushd jpeg-9a
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/libarchive-3.2.1.tar.gz
 pushd libarchive-3.2.1
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/libffi-3.1.tar.gz
 pushd libffi-3.1
 ./configure
 make
-make install
+sudo make install
 popd
 
 # Depends on libffi
@@ -148,7 +148,7 @@ tar xf ../dependency_sources/glib-2.42.0.tar.xz
 pushd glib-2.42.0
 ./configure --enable-static --enable-shared
 make
-make install
+sudo make install
 popd
 
 # Depends on glib
@@ -156,7 +156,7 @@ tar xf ../dependency_sources/atk-2.14.0.tar.xz
 pushd atk-2.14.0
 ./configure
 make
-make install
+sudo make install
 popd
 
 # Depends on glib
@@ -164,7 +164,7 @@ tar xf ../dependency_sources/gdk-pixbuf-2.31.1.tar.xz
 pushd gdk-pixbuf-2.31.1
 ./configure
 make
-make install
+sudo make install
 popd
 
 # Depends on glib
@@ -172,35 +172,35 @@ tar xf ../dependency_sources/gobject-introspection-1.38.0.tar.xz
 pushd gobject-introspection-1.38.0
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/libiconv-1.14.tar.gz
 pushd libiconv-1.14
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/liblo-0.28.tar.gz
 pushd liblo-0.28
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/libogg-1.3.2.tar.xz
 pushd libogg-1.3.2
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/libpng-1.6.37.tar.xz
 pushd libpng-1.6.37
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/libsamplerate-0.1.9.tar.gz
@@ -208,14 +208,14 @@ pushd libsamplerate-0.1.9
 patch -u examples/audio_out.c -i ../../dependency_patches/libsamplerate-0.1.9/examples/audio_out.c.patch
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/libsigc++-2.4.1.tar.xz
 pushd libsigc++-2.4.1
 ./configure
 make
-make install
+sudo make install
 popd
 
 # Depends on glib-2.0 and sigc++-2.0
@@ -223,7 +223,7 @@ tar xf ../dependency_sources/glibmm-2.42.0.tar.xz
 pushd glibmm-2.42.0
 ./configure
 make
-make install
+sudo make install
 popd
 
 # Depends on glibmm-2.4 and atk
@@ -231,14 +231,14 @@ tar xf ../dependency_sources/atkmm-2.22.7.tar.xz
 pushd atkmm-2.22.7
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/libsndfile-1.0.27.tar.gz
 pushd libsndfile-1.0.27
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/libtool-2.4.2.tar.gz
@@ -247,21 +247,21 @@ pushd libtool-2.4.2
 patch -u Makefile.in -i ../../dependency_patches/libtool-2.4.2/Makefile.in.patch
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/libusb-1.0.20.tar.bz2
 pushd libusb-1.0.20
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/libvorbis-1.3.4.tar.xz
 pushd libvorbis-1.3.4
 ./configure
 make
-make install
+sudo make install
 popd
 
 # tar xf ../dependency_sources/libwebsockets-4.3.0-13.tar.gz
@@ -272,7 +272,7 @@ tar xf ../dependency_sources/libxml2-2.9.2.tar.gz
 pushd libxml2-2.9.2
 ./configure
 make
-make install
+sudo make install
 popd
 
 # Depends on intltool >= 0.35.0 and libxml2
@@ -280,7 +280,7 @@ tar xf ../dependency_sources/gnome-doc-utils-0.20.10.tar.xz
 pushd gnome-doc-utils-0.20.10
 ./configure
 make
-make install
+sudo make install
 popd
 
 # Depends on libxml2
@@ -288,28 +288,28 @@ tar xf ../dependency_sources/itstool-2.0.2.tar.bz2
 pushd itstool-2.0.2
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/libxslt-1.1.28.tar.gz
 pushd libxslt-1.1.28
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/lv2-1.18.2.tar.bz2
 pushd lv2-1.18.2
 ./waf configure
 ./waf
-./waf install
+sudo ./waf install
 popd
 
 tar xf ../dependency_sources/m4-1.4.18.tar.bz2
 pushd m4-1.4.18
 ./configure
 make
-make install
+sudo make install
 popd
 
 # tar xf ../dependency_sources/make-4.1.tar.bz2
@@ -324,7 +324,7 @@ tar xf ../dependency_sources/pango-1.40.4.tar.xz
 pushd pango-1.40.4
 ./configure
 make
-make install
+sudo make install
 popd
 
 # Depends on intltool >= 0.31.0 and gtk+
@@ -332,21 +332,21 @@ tar xf ../dependency_sources/gtk-engines-2.21.0.tar.gz
 pushd gtk-engines-2.21.0
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/pcre-8.36.tar.bz2
 pushd pcre-8.36
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/pixman-0.38.4.tar.gz
 pushd pixman-0.38.4
 ./configure
 make
-make install
+sudo make install
 popd
 
 # Depends on pixman-1
@@ -354,7 +354,7 @@ tar xf ../dependency_sources/cairo-1.14.8.tar.xz
 pushd cairo-1.14.8
 ./configure
 make
-make install
+sudo make install
 popd
 
 # Depends on cairo and libsigc++-2.0
@@ -362,7 +362,7 @@ tar xf ../dependency_sources/cairomm-1.11.2.tar.gz
 pushd cairomm-1.11.2
 ./configure
 make
-make install
+sudo make install
 popd
 
 # Depends on glib-2.0, atk, pango, cairo and gdk-pixbuf-2.0
@@ -370,7 +370,7 @@ tar xf ../dependency_sources/gtk+-2.24.23.tar.bz2
 pushd gtk+-2.24.23
 ./configure --enable-relocation
 make
-make install
+sudo make install
 popd
 
 # Depends on cairomm
@@ -378,21 +378,21 @@ tar xf ../dependency_sources/pangomm-2.34.0.tar.xz
 pushd pangomm-2.34.0
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/pkg-config-0.28.tar.gz
 pushd pkg-config-0.28
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/raptor2-2.0.14.tar.gz
 pushd raptor2-2.0.14
 ./configure
 make
-make install
+sudo make install
 popd
 
 # Depends on raptor2
@@ -402,14 +402,14 @@ rm -r m4/*
 autoreconf -i
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/rasqal-0.9.32.tar.gz
 pushd rasqal-0.9.32
 ./configure
 make
-make install
+sudo make install
 popd
 
 # tar xf ../dependency_sources/rdflib-4.1.2.tar.gz
@@ -420,35 +420,35 @@ tar xf ../dependency_sources/readline-7.0.tar.gz
 pushd readline-7.0
 ./configure
 make
-make install
+sudo make install
 popd
 
 # tar xf ../dependency_sources/redland-1.0.17.tar.gz
 # pushd redland-1.0.17
 # ./configure
 # make
-# make install
+# sudo make install
 # popd
 
 tar xf ../dependency_sources/serd-0.30.11.tar.bz2
 pushd serd-0.30.11
 ./waf configure
 ./waf
-./waf install
+sudo ./waf install
 popd
 
 tar xf ../dependency_sources/sord-0.16.9.tar.bz2
 pushd sord-0.16.9
 ./waf configure
 ./waf
-./waf install
+sudo ./waf install
 popd
 
 tar xf ../dependency_sources/sratom-0.6.8.tar.bz2
 pushd sratom-0.6.8
 ./waf configure
 ./waf
-./waf install
+sudo ./waf install
 popd
 
 # Depends on lv2, serd, sord, and sratom
@@ -456,14 +456,14 @@ tar xf ../dependency_sources/lilv-0.24.13.tar.bz2
 pushd lilv-0.24.13
 ./waf configure
 ./waf
-./waf install
+sudo ./waf install
 popd
 
 tar xf ../dependency_sources/suil-0.10.8.tar.bz2
 pushd suil-0.10.8
 ./waf configure
 ./waf
-./waf install
+sudo ./waf install
 popd
 
 # tar xf ../dependency_sources/tar-1.27.tar.bz2
@@ -476,28 +476,28 @@ patch -u configure.in -i ../../dependency_patches/termcap-1.3.1/configure.in.pat
 patch -u tparam.c -i ../../dependency_patches/termcap-1.3.1/tparam.c.patch
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/tiff-4.0.3.tar.gz
 pushd tiff-4.0.3
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/uuid-1.6.2.tar.gz
 pushd uuid-1.6.2
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/vamp-plugin-sdk-2.8.0.tar.gz
 pushd vamp-plugin-sdk-2.8.0
 ./configure
 make
-make install
+sudo make install
 popd
 
 # Depends on vamp-sdk and meson
@@ -511,14 +511,14 @@ tar xf ../dependency_sources/xz-5.2.2.tar.bz2
 pushd xz-5.2.2
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/zlib-1.2.7.tar.gz
 pushd zlib-1.2.7
 ./configure
 make
-make install
+sudo make install
 popd
 
 # Depends on cmake, cppunit, and zlib
@@ -526,7 +526,7 @@ tar xf ../dependency_sources/taglib-1.9.1.tar.gz
 pushd taglib-1.9.1
 cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_RELEASE_TYPE=Release .
 make
-make install
+sudo make install
 popd
 
 
@@ -536,14 +536,14 @@ tar xf ../dependency_sources/gtk-doc-1.21.tar.xz
 pushd gtk-doc-1.21
 ./configure
 make
-make install
+sudo make install
 popd
 
 tar xf ../dependency_sources/gtk-osx-docbook-1.0.tar.gz
 pushd gtk-osx-docbook-1.0
 ./configure
 make
-make install
+sudo make install
 popd
 
 # Depends on giomm-2.4 pangomm-1.4 gtk+-2.0
@@ -551,7 +551,7 @@ tar xf ../dependency_sources/gtkmm-2.24.4.tar.xz
 pushd gtkmm-2.24.4
 ./configure
 make
-make install
+sudo make install
 popd
 
 # Depends on fftw3, libsndfile and libsamplerate
@@ -559,6 +559,6 @@ tar xf ../dependency_sources/aubio-0.3.2.tar.gz
 pushd aubio-0.3.2
 ./configure
 make -j1
-make install
+sudo make install
 popd
 
